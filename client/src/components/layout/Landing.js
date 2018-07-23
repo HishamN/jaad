21db3966cab33f3e606e8fe8faf7bc85b839f88d
Hisham Noman <nourmarket@Hishams-MacBook-Pro.local>
2018-07-23 17:01:32 -0400

    all good

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -17,7 +17,9 @@ class Landing extends Component {
           <div className="container">
             <div className="row">
               <div className="col-md-12 text-center">
-                <h1 className="display-3 mb-4">Developers From the East</h1>
+                <h1 className="display-3 mb-4">
+                  Developers From the East ملتقى المبرمجين العرب
+                </h1>
                 <p className="lead">
                   {" "}
                   Create a developer profile/portfolio, share posts and get help
